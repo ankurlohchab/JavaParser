261965f611c52fccfdcd5a6c5b8348d27b032392
ankur <ankur.lohchab@gmail.com>
2020-05-13 23:44:18 +0200

    implementation to Get args values to engine

--- muni.cz/src/main/java/parser/Engine.java
+++ muni.cz/src/main/java/parser/Engine.java
@@ -1,15 +1,25 @@
 package parser;
 
 public class Engine {
-
-public void run(String[] args)
-{
-parseArgs.validateArgs(args);
-
-
-}
+public String input_loc;
+public String manipulations;
+public String out_type;
+public String output_loc;
 
 
+    public void run(String[] args) {
+    parseArgs.validateArgs(args);
+    getArgsData(args);
+
+
+    }
+
+    private void getArgsData(String[] args) {
+        input_loc = parseArgs.getInputLocation(args);
+        manipulations=parseArgs.getManipulations(args);
+        out_type=parseArgs.getOutType(args);
+        output_loc=parseArgs.getOutLoc(args);
+    }
 
 
 }
