56683f1f9eed6d69c3b3193c295031a906c2d306
ankur <ankur.lohchab@gmail.com>
2020-05-13 22:24:42 +0200

    Scalaton for the project added

---
+++ muni.cz/src/main/java/parser/engine.java
@@ -0,0 +1,12 @@
+package parser;
+
+public class engine {
+    public static void main(String [] args)
+    {
+        //TODO execute all the functionalities in correct order
+    }
+
+
+
+
+}
---
+++ muni.cz/src/main/java/parser/parseArgs.java
@@ -0,0 +1,9 @@
+package parser;
+
+public class parseArgs {
+
+    //TODO validate if the arguments in correct order
+    // TODO flag error if unvalid arguments
+    // TODO Display help for args format
+
+}
